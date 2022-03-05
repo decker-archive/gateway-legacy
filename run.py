34b38060d657f771fe05dc066914964736556130
Vincent <vincentbusiness55@gmail.com>
2022-03-05 13:37:25 +0800

    Limit to 10,000 Ports

--- run.py
+++ run.py
@@ -24,6 +24,8 @@ async def start_gateway():
     for port in range(49151):
         if port < 1024:
             pass
+        if port > 10000:
+            break
         else:
             try:
                 await server.serve(
